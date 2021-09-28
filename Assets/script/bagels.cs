47bc4501dc3ad7f337365d86edc3816b67f13f60
Gabby0sorio <90849848+Gabby0sorio@users.noreply.github.com>
2021-09-28 10:37:32 -0400

    newchagnes

---
+++ Assets/script/bagels.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class bagels:MonoBehaviour
+{
+    public void LoadLevel(string name)
+    {
+        SceneManager.LoadScene(name);
+        
+
+     }
+
+    public void Quit()
+    {
+        Application.Quit();
+    }
+
+
+
+}
+
